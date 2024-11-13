e9b038fedf41ada14f77810b7d123aac0ca47001
Kaylee Xiao <kayleexiao4@gmail.com>
2024-11-13 01:18:34 -0700

    Project setup

---
+++ fit-quest/src/App.js
@@ -0,0 +1,24 @@
+// App.js
+import { Container, MantineProvider } from '@mantine/core';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <MantineProvider theme={{ colorScheme: 'light' }} withGlobalStyles withNormalizeCSS>
+      <Router>
+        <Container style={{ padding: '2rem', textAlign: 'center' }}>
+          <Routes>
+            <Route path="/" element={<ExplorePage />} />
+            <Route path="/calendar" element={<CalenderPage />} />
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/saved" element={<SavedPage />} />
+            <Route path="/workout" element={<WorkoutPage />} />
+          </Routes>
+        </Container>
+      </Router>
+    </MantineProvider>
+  );
+}
+
+export default App;
